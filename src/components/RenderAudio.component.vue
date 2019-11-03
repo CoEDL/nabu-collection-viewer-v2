72a67b229e7b730060b160a6914196d4cb2eee6a
Marco La Rosa <m@lr.id.au>
2019-11-04 10:59:52 +1100

    remove dialog - make it part of card

--- src/components/RenderAudio.component.vue
+++ src/components/RenderAudio.component.vue
@@ -1,52 +1,15 @@
 <template>
-    <div class="my-2">
-        <div class="row">
-            <div class="col text-center">{{audio.name}}</div>
+    <div class="flex flex-col my-2">
+        <div class="text-center">{{audio.name}}</div>
+        <div class="style-audio-icon">
+            <i class="fal fa-volume-up"></i>
         </div>
-        <div class="row">
-            <div class="col text-center style-audio-icon" @click="dialogVisible = !dialogVisible">
-                <i class="fal fa-volume-up"></i>
-            </div>
+        <div class="my-2">
+            <audio controls ref="audioElement" class="style-audio-element">
+                <source :src="item" v-for="(item, idx) of audio.item" :key="idx" />Your browser does not support the
+                <code>audio</code> element.
+            </audio>
         </div>
-
-        <el-dialog
-            :visible.sync="dialogVisible"
-            :fullscreen="true"
-            :before-close="beforeClose"
-            @open="play"
-        >
-            <div class="my-2 text-center">
-                <div class="style-headline text-center style-tile-title">{{audio.title}}</div>
-                <div class="row my-2">
-                    <div class="col style-item-id">
-                        {{audio.collectionId}} /
-                        {{audio.itemId}} /
-                        <item-information
-                            :collectionId="audio.collectionId"
-                            :itemId="audio.itemId"
-                        />
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col">{{audio.name}}</div>
-                </div>
-                <div class="row my-2">
-                    <div class="col style-audio-icon" @click="dialogVisible = !dialogVisible">
-                        <!-- <router-link :to="{ name: 'viewAudio', params: { audio: audio }}"> -->
-                        <i class="fal fa-volume-up"></i>
-                        <!-- </router-link> -->
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col">
-                        <audio controls autoplay ref="audioElement" class="style-audio-element">
-                            <source :src="item" v-for="(item, idx) of audio.item" :key="idx" />Your browser does not support the
-                            <code>audio</code> element.
-                        </audio>
-                    </div>
-                </div>
-            </div>
-        </el-dialog>
     </div>
 </template>
 
@@ -61,20 +24,7 @@ export default {
         ItemInformation
     },
     data() {
-        return {
-            dialogVisible: false
-        };
-    },
-    methods: {
-        beforeClose() {
-            this.$refs["audioElement"].pause();
-            this.dialogVisible = false;
-        },
-        play() {
-            setTimeout(() => {
-                this.$refs["audioElement"].play();
-            }, 500);
-        }
+        return {};
     }
 };
 </script>
@@ -82,7 +32,7 @@ export default {
 <style lang="scss" scoped>
 .style-audio-icon {
     cursor: pointer;
-    font-size: 5em;
+    font-size: 3em;
 }
 
 .style-audio-element {
