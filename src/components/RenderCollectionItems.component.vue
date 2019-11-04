7570990abb729e37d6ffb8368bad7b4707c46c43
Marco La Rosa <m@lr.id.au>
2019-11-04 17:33:43 +1100

    implement filtering of collection items

--- src/components/RenderCollectionItems.component.vue
+++ src/components/RenderCollectionItems.component.vue
@@ -1,5 +1,27 @@
 <template>
     <div>
+        <div class="my-2">
+            <div class="text-sm">Filter items:</div>
+            <div class="flex flex-row flex-wrap">
+                <el-button
+                    @click="filterItems = []"
+                    size="mini"
+                    type="text"
+                    :disabled="!filterItems.length"
+                >
+                    <i class="fas fa-ban"></i>&nbsp;reset filters
+                </el-button>
+
+                <el-tag
+                    v-for="(item, idx) of collectionItemIds"
+                    :key="idx"
+                    class="cursor-pointer m-1"
+                    :size="item.size"
+                    :type="item.type"
+                    @click="toggleItem(item)"
+                >{{item.itemId}}</el-tag>
+            </div>
+        </div>
         <div v-for="(chunk, idx) of chunks" :key="idx">
             <div class="flex flex-row items-center">
                 <div v-for="(item, idx) of chunk" :key="idx" class="flex-1 m-1">
@@ -11,7 +33,7 @@
 </template>
 
 <script>
-import { chunk } from "lodash";
+import { chunk, uniq, without } from "lodash";
 import RenderItemComponent from "./RenderItem.component.vue";
 
 export default {
@@ -20,23 +42,53 @@ export default {
     },
     data() {
         return {
-            nChunks: 1
+            nChunks: 1,
+            filterItems: []
         };
     },
     computed: {
-        chunks: function() {
-            let items = this.$store.getters.itemsFlattened.filter(
+        items: function() {
+            return this.$store.getters.itemsFlattened.filter(
+                item =>
+                    item.collectionId === this.$route.params.collectionId &&
+                    !this.filterItems.includes(item.itemId)
+            );
+        },
+        itemFilters: function() {
+            return this.$store.getters.itemsFlattened.filter(
                 item => item.collectionId === this.$route.params.collectionId
             );
+        },
+        chunks: function() {
             if (window.innerWidth > 400 && window.innerWidth <= 1024) {
                 this.nChunks = 2;
             } else if (window.innerWidth > 1024) {
                 this.nChunks = 3;
             }
-            return chunk(items, this.nChunks);
+            return chunk(this.items, this.nChunks);
+        },
+        collectionItemIds: function() {
+            const itemIds = uniq(
+                this.itemFilters.map(item => item.itemId)
+            ).sort();
+            return itemIds.map(itemId => {
+                return {
+                    itemId: itemId,
+                    type: this.filterItems.includes(itemId)
+                        ? "warning"
+                        : "success",
+                    size: window.innerWidth < 768 ? "mini" : "medium"
+                };
+            });
         }
     },
-    mounted() {}
+    methods: {
+        toggleItem(item) {
+            this.filterItems = this.filterItems.includes(item.itemId)
+                ? without(this.filterItems, item.itemId)
+                : [...this.filterItems, item.itemId];
+        }
+    }
 };
 </script>
 
