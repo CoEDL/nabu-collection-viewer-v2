e946fd7597537cc1131eb97ffa9187589dd4ee9a
Marco La Rosa <m@lr.id.au>
2019-11-11 10:16:31 +1100

    add pagination

--- src/components/RenderCollectionList.component.vue
+++ src/components/RenderCollectionList.component.vue
@@ -1,5 +1,12 @@
 <template>
     <div class="remove-padding m-2">
+        <el-pagination
+            :small="small"
+            layout="prev, pager,  next"
+            :page-size.sync="pageSize"
+            :current-page.sync="page"
+            :total="totalItems"
+        ></el-pagination>
         <div v-for="(chunk, idx) of chunks" :key="idx">
             <div class="flex flex-row items-center">
                 <div v-for="collection of chunk" :key="collection.collectionId" class="flex-1 m-1">
@@ -22,17 +29,28 @@ export default {
     },
     data() {
         return {
-            nChunks: 1
+            nChunks: 1,
+            page: 0,
+            pageSize: 10,
+            small: window.innerWidth < 400 ? true : false
         };
     },
     computed: {
+        totalItems: function() {
+            return this.$store.state.collections.length;
+        },
         chunks: function() {
             if (window.innerWidth > 400 && window.innerWidth <= 1024) {
                 this.nChunks = 2;
             } else if (window.innerWidth > 1024) {
                 this.nChunks = 3;
+                this.pageSize = 9;
             }
-            return chunk(this.$store.state.collections, this.nChunks);
+            let collections = this.$store.state.collections.slice(
+                (this.page - 1) * this.pageSize,
+                (this.page - 1) * this.pageSize + this.pageSize
+            );
+            return chunk(collections, this.nChunks);
         }
     }
 };
