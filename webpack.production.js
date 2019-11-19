02c2395f5d1a33328e59774e26a51848bf570737
Marco La Rosa <m@lr.id.au>
2019-11-19 15:33:14 +1100

    rework production build configuration for css; formatting

--- webpack.production.js
+++ webpack.production.js
@@ -1,102 +1,97 @@
-'use strict';
+"use strict";
 
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
-const VueLoaderPlugin = require('vue-loader/lib/plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 module.exports = {
-    target: 'web',
-    mode: 'production',
-    devtool: 'none',
-    entry: ['./src/vendor.js', './src/index.js'],
+    target: "web",
+    mode: "production",
+    devtool: "none",
+    entry: ["./src/vendor.js", "./src/index.js"],
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[hash].bundle.js',
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].[contenthash].bundle.js"
     },
     optimization: {
-        moduleIds: 'hashed',
-        runtimeChunk: 'single',
+        moduleIds: "hashed",
+        runtimeChunk: "single",
         splitChunks: {
             cacheGroups: {
                 vendor: {
                     test: /[\\/]node_modules[\\/]/,
-                    name: 'vendors',
-                    chunks: 'all',
-                },
-            },
+                    name: "vendors",
+                    chunks: "all"
+                }
+            }
         },
-        minimizer: [new TerserPlugin(), new OptimizeCSSAssetsPlugin({})],
+        minimizer: [new TerserPlugin(), new OptimizeCSSAssetsPlugin({})]
     },
     plugins: [
         new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify('production'),
+            "process.env.NODE_ENV": JSON.stringify("production")
         }),
         new MiniCssExtractPlugin({
-            filename: '[name].css',
-            chunkFilename: '[id].css',
+            filename: "[name].[contenthash].css"
         }),
         new CleanWebpackPlugin({
-            cleanOnceBeforeBuildPatterns: ['*.js', '*.css'],
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].[contenthash].css',
+            cleanOnceBeforeBuildPatterns: ["*.js", "*.css", "*.LICENSE"]
         }),
         new HtmlWebpackPlugin({
-            title: 'Mobile Collection Viewer',
-            template: './src/index.html',
+            title: "Mobile Collection Viewer",
+            template: "./src/index.html"
         }),
-        new VueLoaderPlugin(),
+        new VueLoaderPlugin()
     ],
     module: {
         rules: [
             {
                 test: /\.vue$/,
-                loader: 'vue-loader',
+                loader: "vue-loader"
             },
             {
                 test: /\.js$/,
-                loader: 'babel-loader',
+                loader: "babel-loader",
                 exclude: /node_modules/,
-                query: {compact: false},
+                query: { compact: false }
             },
             {
                 test: /\.css$/,
                 use: [
-                    'vue-style-loader',
-                    {loader: 'css-loader', options: {importLoaders: 1}},
-                    'postcss-loader',
-                ],
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "postcss-loader"
+                ]
             },
             {
                 test: /\.scss$/,
                 use: [
-                    'vue-style-loader',
-                    {loader: 'css-loader', options: {importLoaders: 1}},
-                    'postcss-loader',
-                    'sass-loader',
-                ],
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "postcss-loader",
+                    "sass-loader"
+                ]
             },
             {
                 test: /\.(woff|woff2|ttf|eot|svg|png|jp(e*)g|gif)?$/,
-                loader: 'file-loader?name=res/[name].[ext]?[hash]',
-            },
-        ],
+                loader: "file-loader?name=res/[name].[ext]?[hash]"
+            }
+        ]
     },
     resolve: {
         alias: {
-            src: path.resolve(__dirname, 'src'),
-            assets: path.resolve(__dirname, 'src/assets'),
-            components: path.resolve(__dirname, 'src/components'),
-            configuration: path.resolve(__dirname, 'src/configuration'),
-            directives: path.resolve(__dirname, 'src/directives'),
-            routes: path.resolve(__dirname, 'src/routes/'),
-            services: path.resolve(__dirname, 'src/services'),
-            store: path.resolve(__dirname, 'src/store'),
-        },
-    },
+            src: path.resolve(__dirname, "src"),
+            assets: path.resolve(__dirname, "src/assets"),
+            components: path.resolve(__dirname, "src/components"),
+            configuration: path.resolve(__dirname, "src/configuration"),
+            directives: path.resolve(__dirname, "src/directives"),
+            routes: path.resolve(__dirname, "src/routes/"),
+            services: path.resolve(__dirname, "src/services"),
+            store: path.resolve(__dirname, "src/store")
+        }
+    }
 };
